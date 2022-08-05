724cc22e3d2ca8e4e59917048a3c7349a558f646
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2022-08-05 16:52:03 +0530

    black and bright blue
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

--- config.def.h
+++ config.def.h
@@ -142,7 +142,7 @@ static const char *altcolorname[] = {
     * foreground, background, cursor, reverse cursor
  */
    unsigned int defaultfg = 8;
-   unsigned int defaultbg = 4;
+   unsigned int defaultbg = 12;
 static unsigned int defaultcs = 14;
 static unsigned int defaultrcs = 15;
 
